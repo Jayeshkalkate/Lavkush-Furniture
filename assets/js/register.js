d4ddce77f77a547f21139283a38be466abe8133f
Jayesh Kalkate <Kalkatejayesh@gmail.com>
2024-12-25 15:34:52 +0530

    Version 1.3

--- assets/js/register.js
+++ assets/js/register.js
@@ -7,9 +7,7 @@ document.getElementById('registerForm').addEventListener('submit', async functio
 
     if (name && email && password) {
         try {
-            // http://127.0.0.1:5500/register.html
-            // const response = await fetch('http://127.0.0.1:5000/register', {
-                const response = await fetch('https://lavkush-furniture.netlify.app/register', {
+            const response = await fetch('https://lavkush-furniture.netlify.app/register', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
